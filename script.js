e1cc9357ae16df03c0d31101c6dc1e0498e8e101
Jose <jm.pagan@gmail.com>
2017-05-09 17:57:00 +0100

    donegit add *!

--- script.js
+++ script.js
@@ -1,42 +1,53 @@
+// google api font  AIzaSyCbrVLRx6QQPSTludd-DKqYCMhDW2cPYVc
+
+
 var cita;
 var twitLink
 $(function() {
-  // jQuery goes here...
+    // jQuery goes here...
 
-$.ajaxSetup({
-  headers : {
-    "X-Mashape-Key": "VCEFjjDSvCmshrETndUi8NVoj2Snp1Z225wjsnQf4B0CQhl8zb",
-    "Content-Type": "application/x-www-form-urlencoded",
-    "Accept": "application/json"
-  }
-});
+    $.ajaxSetup({
+        headers: {
+            "X-Mashape-Key": "VCEFjjDSvCmshrETndUi8NVoj2Snp1Z225wjsnQf4B0CQhl8zb",
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Accept": "application/json"
+        }
+    });
 
-$(document).ready(function(){
+    $(document).ready(function() {
 
-  $("#btn-click").click(function(){
+        $("#btn-click").click(function() {
+            $("#twitbtn").remove();
+            $("#author").fadeTo("slow",0);
+            $("#quoteText").fadeTo("slow",0);
 
-$.getJSON("https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous&count=1", function(data) {
-  //  console.log(data);
-cita = data;
-
-   $("#author").text(data.author);
-   $("#quoteText").text(data.quote);
+            $.getJSON("https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous&count=1", function(data) {
 
 
-twitLink = "https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=" + encodeURI(data.quote);
-console.log(twitLink);
+                $("#author").text(data.author).fadeTo("slow",1);
 
+                $("#quoteText").text(data.quote).fadeTo("slow",1);
 
-// learn about web-intent:
-// https://dev.twitter.com/web/tweet-button/web-intent
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/encodeURI
-
-console.log(twitLink);
+                var quoteAuthor = data.quote+" ("+ data.author+")"
 
 
 
+                twitLink = "https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=" + encodeURI(quoteAuthor);
 
-});
-});
-});
+                $(".card-block").append("<a id='twitbtn' class='btn btn-primary' href='" + twitLink + "' role='button'>Tweet Quote</a>");
+
+
+
+                // learn about web-intent:
+                // https://dev.twitter.com/web/tweet-button/web-intent
+                // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/encodeURI
+
+                console.log(twitLink);
+
+
+
+
+            });
+        });
+    });
 });
